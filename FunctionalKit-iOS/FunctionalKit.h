43cbfff1652b1356894ae9d05130ec2326ff5d56
Elviro Rocca <elviro.rocca@facile.it>
2017-11-16 11:57:00 +0100

    added Operadics dependency

---
+++ FunctionalKit-iOS/FunctionalKit.h
@@ -0,0 +1,18 @@
+//
+//  FunctionalKit_iOS.h
+//  FunctionalKit-iOS
+//
+//  Created by Elviro Rocca on 16/11/17.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FunctionalKit_iOS.
+FOUNDATION_EXPORT double FunctionalKit_iOSVersionNumber;
+
+//! Project version string for FunctionalKit_iOS.
+FOUNDATION_EXPORT const unsigned char FunctionalKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FunctionalKit_iOS/PublicHeader.h>
+
+
--- Package.swift
+++ Package.swift
@@ -12,17 +12,17 @@ let package = Package(
             targets: ["FunctionalKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+		.package(url: "https://github.com/typelift/Operadics.git",
+				 from: Version(0,0,0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FunctionalKit",
-            dependencies: []),
+            dependencies: ["Operadics"]),
         .testTarget(
             name: "FunctionalKitTests",
-            dependencies: ["FunctionalKit"]),
+            dependencies: ["FunctionalKit","Operadics"]),
     ]
 )
