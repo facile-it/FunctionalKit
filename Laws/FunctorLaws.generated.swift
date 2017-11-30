29cef3b2a803d76e24537f0c2c020788517fe806
Christian Miranti <christian.miranti@facile.it>
2017-11-30 16:41:47 +0100

    FPI-18 some improvements

--- Laws/FunctorLaws.generated.swift
+++ Laws/FunctorLaws.generated.swift
@@ -1,203 +1,144 @@
 // Generated using Sourcery 0.7.2 — https://github.com/krzysztofzablocki/Sourcery
 // DO NOT EDIT
 
-
-
-
-
-
-
-
-
 import XCTest
 @testable import FunctionalKit
 import SwiftCheck
+import Abstract
 
 class FunctorLawsTests: XCTestCase {
-
 
 //MARK: Array
 
 // Identity Law
     func testArrayIdentity() {
-        property("Array - Functor Laws - Identity") <- forAll { (x: ArrayOf<Int>) in
+        property("Array - Functor Laws - Identity") <- forAll { (x: ArrayOf<String>) in
             return x.getArray.map(fidentity) == fidentity(x.getArray)
         }
     }
 
-
-
-
-
-
 //MARK: Coproduct
-
 
 // Left Identity Law
     func testCoproductLeftIdentity() {
-        property("Coproduct - Functor Laws - Left Identity") <- forAll { (x: Int) in
-            return Coproduct<Int,Int>.left(x).mapLeft(fidentity) == fidentity(Coproduct<Int,Int>.left(x))
+        property("Coproduct - Functor Laws - Left Identity") <- forAll { (x: String) in
+            return Coproduct<String,String>.left(x).mapLeft(fidentity) == fidentity(Coproduct<String,String>.left(x))
         }
     }
 
 // Right Identity Law
     func testCoproductRightIdentity() {
-        property("Coproduct - Functor Laws - Right Identity") <- forAll { (x: Int) in
-            return Coproduct<Int,Int>.right(x).mapRight(fidentity) == fidentity(Coproduct<Int,Int>.right(x))
+        property("Coproduct - Functor Laws - Right Identity") <- forAll { (x: String) in
+            return Coproduct<String,String>.right(x).mapRight(fidentity) == fidentity(Coproduct<String,String>.right(x))
         }
     }
-
-
-
 
 //MARK: Exponential
 
 // Identity Law
     func testExponentialIdentity() {
-        property("Exponential - Functor Laws - Identity") <- forAll { (x: ArrowOf<Int,Int>, context: Int) in
+        property("Exponential - Functor Laws - Identity") <- forAll { (x: ArrowOf<String,String>, context: String) in
             return (Exponential.init(x.getArrow).map(fidentity) == fidentity(Exponential.init(x.getArrow))).run(context)
         }
     }
-
-
-
-
-
 
 //MARK: Future
 
 // Identity Law
     func testFutureIdentity() {
-        property("Future - Functor Laws - Identity") <- forAll { (x: Int) in
+        property("Future - Functor Laws - Identity") <- forAll { (x: String) in
             return Future.unfold({ $0(x) }).map(fidentity) == fidentity(Future.unfold({ $0(x) }))
         }
     }
 
-
-
-
-
-
 //MARK: Inclusive
-
 
 // Left Identity Law
     func testInclusiveLeftIdentity() {
-        property("Inclusive - Functor Laws - Left Identity") <- forAll { (x: Int, y: Int) in
-            return Inclusive<Int,Int>.left(x).mapLeft(fidentity) == fidentity(Inclusive<Int,Int>.left(x))
+        property("Inclusive - Functor Laws - Left Identity") <- forAll { (x: String, y: String) in
+            return Inclusive<String,String>.left(x).mapLeft(fidentity) == fidentity(Inclusive<String,String>.left(x))
         }
     }
 
 // Right Identity Law
     func testInclusiveRightIdentity() {
-        property("Inclusive - Functor Laws - Right Identity") <- forAll { (x: Int, y: Int) in
-            return Inclusive<Int,Int>.right(x).mapRight(fidentity) == fidentity(Inclusive<Int,Int>.right(x))
+        property("Inclusive - Functor Laws - Right Identity") <- forAll { (x: String, y: String) in
+            return Inclusive<String,String>.right(x).mapRight(fidentity) == fidentity(Inclusive<String,String>.right(x))
         }
     }
 
     func testInclusiveCenterIdentity() {
-        property("Inclusive - Functor Laws - Center Identity") <- forAll { (x: Int, y: Int) in
-            return Inclusive<Int,Int>.center(x,y).bimap(onLeft: fidentity, onRight: fidentity) == fidentity(Inclusive<Int,Int>.center(x,y))
+        property("Inclusive - Functor Laws - Center Identity") <- forAll { (x: String, y: String) in
+            return Inclusive<String,String>.center(x,y).bimap(onLeft: fidentity, onRight: fidentity) == fidentity(Inclusive<String,String>.center(x,y))
         }
     }
-
-
 
 //MARK: Optional
 
 // Identity Law
     func testOptionalIdentity() {
-        property("Optional - Functor Laws - Identity") <- forAll { (x: OptionalOf<Int>) in
+        property("Optional - Functor Laws - Identity") <- forAll { (x: OptionalOf<String>) in
             return x.getOptional.map(fidentity) == fidentity(x.getOptional)
         }
     }
 
-
-
-
-
-
 //MARK: Product
-
-
-
-
 
 // First Identity Law
     func testProductFirstIdentity() {
-        property("Product - Functor Laws - First Identity") <- forAll { (x: Int, y: Int) in
-            return Product<Int,Int>.init(x,y).mapFirst(fidentity) == fidentity(Product<Int,Int>.init(x,y))
+        property("Product - Functor Laws - First Identity") <- forAll { (x: String, y: String) in
+            return Product<String,String>.init(x,y).mapFirst(fidentity) == fidentity(Product<String,String>.init(x,y))
         }
     }
 
 // Second Identity Law
     func testProductSecondIdentity() {
-        property("Product - Functor Laws - Second Identity") <- forAll { (x: Int, y: Int) in
-            return Product<Int,Int>.init(x,y).mapSecond(fidentity) == fidentity(Product<Int,Int>.init(x,y))
+        property("Product - Functor Laws - Second Identity") <- forAll { (x: String, y: String) in
+            return Product<String,String>.init(x,y).mapSecond(fidentity) == fidentity(Product<String,String>.init(x,y))
         }
     }
 
 // Both Identity Law
     func testProductBothIdentity() {
-        property("Product - Functor Laws - Both Identity") <- forAll { (x: Int, y: Int) in
-            return Product<Int,Int>.init(x,y).bimap(onFirst: fidentity, onSecond: fidentity) == fidentity(Product<Int,Int>.init(x,y))
+        property("Product - Functor Laws - Both Identity") <- forAll { (x: String, y: String) in
+            return Product<String,String>.init(x,y).bimap(onFirst: fidentity, onSecond: fidentity) == fidentity(Product<String,String>.init(x,y))
         }
     }
-
 
 //MARK: Reader
 
 // Identity Law
     func testReaderIdentity() {
-        property("Reader - Functor Laws - Identity") <- forAll { (x: ArrowOf<Int,Int>, context: Int) in
+        property("Reader - Functor Laws - Identity") <- forAll { (x: ArrowOf<String,String>, context: String) in
             return (Reader.unfold(x.getArrow).map(fidentity) == fidentity(Reader.unfold(x.getArrow))).run(context)
         }
     }
-
-
-
-
-
 
 //MARK: Result
 
 // Identity Law
     func testResultIdentity() {
-        property("Result - Functor Laws - Identity") <- forAll { (x: Int) in
-            return Result<AnyError,Int>.success(x).map(fidentity) == fidentity(Result<AnyError,Int>.success(x))
+        property("Result - Functor Laws - Identity") <- forAll { (x: String) in
+            return Result<AnyError,String>.success(x).map(fidentity) == fidentity(Result<AnyError,String>.success(x))
         }
     }
-
-
-
-
-
 
 //MARK: State
 
 // Identity Law
     func testStateIdentity() {
-        property("State - Functor Laws - Identity") <- forAll { (x: Int, context: Int) in
-            return (State<Int,Int>.unfold{ s in (s,x)}.map(fidentity) == fidentity(State<Int,Int>.unfold{ s in (s,x)})).run(context)
+        property("State - Functor Laws - Identity") <- forAll { (x: String, context: String) in
+            return (State<String,String>.unfold{ s in (s,x)}.map(fidentity) == fidentity(State<String,String>.unfold{ s in (s,x)})).run(context)
         }
     }
-
-
-
-
-
 
 //MARK: Writer
 
 // Identity Law
     func testWriterIdentity() {
-        property("Writer - Functor Laws - Identity") <- forAll { (x: Int, context: String) in
-            return Writer<String,Int>.init(log: context, value: x).map(fidentity) == fidentity(Writer<String,Int>.init(log: context, value: x))
+        property("Writer - Functor Laws - Identity") <- forAll { (x: String, context: String) in
+            return Writer<String,String>.init(log: context, value: x).map(fidentity) == fidentity(Writer<String,String>.init(log: context, value: x))
         }
     }
-
-
-
-
 
 }
