9a97a80bdcd38b1c58e0302b2ed51802d5557eb5
rpallas92 <ricardo.pallas@adidas-group.com>
2017-12-02 17:03:00 +0100

    Added lift tests for Array Type

---
+++ Tests/FunctionalKitTests/ArrayTests.swift
@@ -0,0 +1,42 @@
+import XCTest
+import Operadics
+@testable import FunctionalKit
+
+class ArrayTests: XCTestCase {
+    
+    
+    func testLiftOneArg() {
+        let id: (Int) -> Int = {$0}
+        let ap1 = Array.pure(1)
+        
+        XCTAssert(Array.lift(id)(ap1) == (Array.pure(id) <*> ap1))
+        XCTAssert(Array.lift(id)(ap1) == Array.pure(1))
+    }
+    
+    func testLiftTwoArgs() {
+        let sum: (Int,Int) -> Int = {$0 + $1}
+        let ap1 = Array.pure(1)
+        let ap2 = Array.pure(2)
+        
+        XCTAssert(Array.lift(sum)(ap1, ap2) == (Array.pure(fcurry(sum)) <*> ap1 <*> ap2))
+        XCTAssert(Array.lift(sum)(ap1, ap2) == Array.pure(3))
+    }
+    
+    func testLiftThreeArgs(){
+        let sum: (Int,Int, Int) -> Int = {$0 + $1 + $2}
+        let ap1 = Array.pure(1)
+        let ap2 = Array.pure(2)
+        let ap3 = Array.pure(3)
+        
+        XCTAssert(Array.lift(sum)(ap1, ap2, ap3) == (Array.pure(fcurry(sum)) <*> ap1 <*> ap2 <*> ap3))
+        XCTAssert(Array.lift(sum)(ap1, ap2, ap3) == Array.pure(6))
+    }
+    
+    static var allTests = [
+        ("testLiftOneArg", testLiftOneArg),
+        ("testLiftTwoArgs", testLiftTwoArgs),
+        ("testLiftThreeArgs", testLiftThreeArgs)
+    ]
+}
+
+
--- Tests/FunctionalKitTests/OptionalTests.swift
+++ Tests/FunctionalKitTests/OptionalTests.swift
@@ -7,29 +7,29 @@ class OptionalTests: XCTestCase {
     
     func testLiftOneArg() {
         let id: (Int) -> Int = {$0}
-        let op1 = Optional.pure(1)
+        let ap1 = Optional.pure(1)
         
-        XCTAssert(Optional.lift(id)(op1) == (Optional.pure(id) <*> op1))
-        XCTAssert(Optional.lift(id)(op1) == Optional.pure(1))
+        XCTAssert(Optional.lift(id)(ap1) == (Optional.pure(id) <*> ap1))
+        XCTAssert(Optional.lift(id)(ap1) == Optional.pure(1))
     }
     
     func testLiftTwoArgs() {
         let sum: (Int,Int) -> Int = {$0 + $1}
-        let op1 = Optional.pure(1)
-        let op2 = Optional.pure(2)
-        
-        XCTAssert(Optional.lift(sum)(op1, op2) == (Optional.pure(fcurry(sum)) <*> op1 <*> op2))
-        XCTAssert(Optional.lift(sum)(op1, op2) == Optional.pure(3))
+        let ap1 = Optional.pure(1)
+        let ap2 = Optional.pure(2)
+
+        XCTAssert(Optional.lift(sum)(ap1, ap2) == (Optional.pure(fcurry(sum)) <*> ap1 <*> ap2))
+        XCTAssert(Optional.lift(sum)(ap1, ap2) == Optional.pure(3))
     }
     
     func testLiftThreeArgs(){
         let sum: (Int,Int, Int) -> Int = {$0 + $1 + $2}
-        let op1 = Optional.pure(1)
-        let op2 = Optional.pure(2)
-        let op3 = Optional.pure(3)
+        let ap1 = Optional.pure(1)
+        let ap2 = Optional.pure(2)
+        let ap3 = Optional.pure(3)
         
-        XCTAssert(Optional.lift(sum)(op1, op2, op3) == (Optional.pure(fcurry(sum)) <*> op1 <*> op2 <*> op3))
-        XCTAssert(Optional.lift(sum)(op1, op2, op3) == Optional.pure(6))
+        XCTAssert(Optional.lift(sum)(ap1, ap2, ap3) == (Optional.pure(fcurry(sum)) <*> ap1 <*> ap2 <*> ap3))
+        XCTAssert(Optional.lift(sum)(ap1, ap2, ap3) == Optional.pure(6))
     }
     
     static var allTests = [
