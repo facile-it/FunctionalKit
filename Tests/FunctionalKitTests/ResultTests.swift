61957f21604ec8294be0783ca21a83f0ac0fced9
rpallas92 <ricardo.pallas@adidas-group.com>
2017-12-02 17:08:20 +0100

    Added lift tests for Result Type

---
+++ Tests/FunctionalKitTests/ResultTests.swift
@@ -0,0 +1,47 @@
+import XCTest
+import Operadics
+@testable import FunctionalKit
+
+class ResultTests: XCTestCase {
+    
+    enum TestErrorType: Error {
+        case error
+    }
+    
+    typealias TestResultType<T> = Result<TestErrorType, T>
+    
+    func testLiftOneArg() {
+        let id: (Int) -> Int = {$0}
+        let ap1 = TestResultType.pure(1)
+        
+        XCTAssert(TestResultType.lift(id)(ap1) == (TestResultType.pure(id) <*> ap1))
+        XCTAssert(TestResultType.lift(id)(ap1) == TestResultType.pure(1))
+    }
+    
+    func testLiftTwoArgs() {
+        let sum: (Int,Int) -> Int = {$0 + $1}
+        let ap1 = TestResultType.pure(1)
+        let ap2 = TestResultType.pure(2)
+        
+        XCTAssert(TestResultType.lift(sum)(ap1, ap2) == (TestResultType.pure(fcurry(sum)) <*> ap1 <*> ap2))
+        XCTAssert(TestResultType.lift(sum)(ap1, ap2) == TestResultType.pure(3))
+    }
+    
+    func testLiftThreeArgs(){
+        let sum: (Int,Int, Int) -> Int = {$0 + $1 + $2}
+        let ap1 = TestResultType.pure(1)
+        let ap2 = TestResultType.pure(2)
+        let ap3 = TestResultType.pure(3)
+        
+        XCTAssert(TestResultType.lift(sum)(ap1, ap2, ap3) == (TestResultType.pure(fcurry(sum)) <*> ap1 <*> ap2 <*> ap3))
+        XCTAssert(TestResultType.lift(sum)(ap1, ap2, ap3) == TestResultType.pure(6))
+    }
+    
+    static var allTests = [
+        ("testLiftOneArg", testLiftOneArg),
+        ("testLiftTwoArgs", testLiftTwoArgs),
+        ("testLiftThreeArgs", testLiftThreeArgs)
+    ]
+}
+
+
