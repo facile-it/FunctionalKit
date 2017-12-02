1a97e312898cfb1dcd503dce3573537996329f96
rpallas92 <ricardo.pallas@adidas-group.com>
2017-12-02 16:57:43 +0100

    Added more lift test for Optional Type

--- Tests/FunctionalKitTests/OptionalTests.swift
+++ Tests/FunctionalKitTests/OptionalTests.swift
@@ -4,7 +4,16 @@ import Operadics
 
 class OptionalTests: XCTestCase {
     
-    func testLift() {
+    
+    func testLiftOneArg() {
+        let id: (Int) -> Int = {$0}
+        let op1 = Optional.pure(1)
+        
+        XCTAssert(Optional.lift(id)(op1) == (Optional.pure(id) <*> op1))
+        XCTAssert(Optional.lift(id)(op1) == Optional.pure(1))
+    }
+    
+    func testLiftTwoArgs() {
         let sum: (Int,Int) -> Int = {$0 + $1}
         let op1 = Optional.pure(1)
         let op2 = Optional.pure(2)
@@ -13,8 +22,20 @@ class OptionalTests: XCTestCase {
         XCTAssert(Optional.lift(sum)(op1, op2) == Optional.pure(3))
     }
     
+    func testLiftThreeArgs(){
+        let sum: (Int,Int, Int) -> Int = {$0 + $1 + $2}
+        let op1 = Optional.pure(1)
+        let op2 = Optional.pure(2)
+        let op3 = Optional.pure(3)
+        
+        XCTAssert(Optional.lift(sum)(op1, op2, op3) == (Optional.pure(fcurry(sum)) <*> op1 <*> op2 <*> op3))
+        XCTAssert(Optional.lift(sum)(op1, op2, op3) == Optional.pure(6))
+    }
+    
     static var allTests = [
-        ("testLift", testLift)
+        ("testLiftOneArg", testLiftOneArg),
+        ("testLiftTwoArgs", testLiftTwoArgs),
+        ("testLiftThreeArgs", testLiftThreeArgs)
         ]
 }
 
