008480e46f461bd1a2aab66e42dcb4723b4076d5
Elviro Rocca <elviro.rocca@facile.it>
2017-12-04 14:46:41 +0100

    FPI-18 completed applicative laws

--- Tests/FunctionalKitTests/LawsTests.generated.swift
+++ Tests/FunctionalKitTests/LawsTests.generated.swift
@@ -13,6 +13,7 @@ import Abstract
 class LawsTests: XCTestCase {
 
 //MARK: Array
+
     func testArrayFunctorIdentity() {
         property("Array - Functor Laws - Identity") <- forAll { (x: String) in
             let a = Array<String>.init([x])
@@ -55,8 +56,19 @@ class LawsTests: XCTestCase {
         }
     }
 
+    func testArrayApplicativeComposition() {
+        property("Array - Applicative Laws - Interchange") <- forAll { (af: ArrowOf<String,String>, ag: ArrowOf<String,String>, x: String) in
+            let a = Array<String>.pure(x)
+            let a_a1 = Array<Endo<String>>.pure(af.getArrow)
+            let a_a2 = Array<Endo<String>>.pure(ag.getArrow)
+            let a_a_a = Array<(@escaping Endo<String>) -> (@escaping Endo<String>) -> Endo<String>>.pure(fcurry(fcompose))
+            return ((a_a_a <*> a_a1 <*> a_a2 <*> a) == (a_a2 <*> (a_a1 <*> a)))
+        }
+    }
+
 
 //MARK: Future
+
     func testFutureFunctorIdentity() {
         property("Future - Functor Laws - Identity") <- forAll { (x: String) in
             let a = Future<String>.unfold { $0(x) }
@@ -99,8 +111,19 @@ class LawsTests: XCTestCase {
         }
     }
 
+    func testFutureApplicativeComposition() {
+        property("Future - Applicative Laws - Interchange") <- forAll { (af: ArrowOf<String,String>, ag: ArrowOf<String,String>, x: String) in
+            let a = Future<String>.pure(x)
+            let a_a1 = Future<Endo<String>>.pure(af.getArrow)
+            let a_a2 = Future<Endo<String>>.pure(ag.getArrow)
+            let a_a_a = Future<(@escaping Endo<String>) -> (@escaping Endo<String>) -> Endo<String>>.pure(fcurry(fcompose))
+            return ((a_a_a <*> a_a1 <*> a_a2 <*> a).start() == (a_a2 <*> (a_a1 <*> a)).start())
+        }
+    }
+
 
 //MARK: Optional
+
     func testOptionalFunctorIdentity() {
         property("Optional - Functor Laws - Identity") <- forAll { (x: String) in
             let a = Optional<String>.init(x)
@@ -143,8 +166,19 @@ class LawsTests: XCTestCase {
         }
     }
 
+    func testOptionalApplicativeComposition() {
+        property("Optional - Applicative Laws - Interchange") <- forAll { (af: ArrowOf<String,String>, ag: ArrowOf<String,String>, x: String) in
+            let a = Optional<String>.pure(x)
+            let a_a1 = Optional<Endo<String>>.pure(af.getArrow)
+            let a_a2 = Optional<Endo<String>>.pure(ag.getArrow)
+            let a_a_a = Optional<(@escaping Endo<String>) -> (@escaping Endo<String>) -> Endo<String>>.pure(fcurry(fcompose))
+            return ((a_a_a <*> a_a1 <*> a_a2 <*> a) == (a_a2 <*> (a_a1 <*> a)))
+        }
+    }
+
 
 //MARK: Reader
+
     func testReaderFunctorIdentity() {
         property("Reader - Functor Laws - Identity") <- forAll { (x: String, c: String) in
             let a = Reader<String,String>.unfold { _ in x }
@@ -187,8 +221,19 @@ class LawsTests: XCTestCase {
         }
     }
 
+    func testReaderApplicativeComposition() {
+        property("Reader - Applicative Laws - Interchange") <- forAll { (af: ArrowOf<String,String>, ag: ArrowOf<String,String>, x: String, c: String) in
+            let a = Reader<String,String>.pure(x)
+            let a_a1 = Reader<String,Endo<String>>.pure(af.getArrow)
+            let a_a2 = Reader<String,Endo<String>>.pure(ag.getArrow)
+            let a_a_a = Reader<String,(@escaping Endo<String>) -> (@escaping Endo<String>) -> Endo<String>>.pure(fcurry(fcompose))
+            return ((a_a_a <*> a_a1 <*> a_a2 <*> a) == (a_a2 <*> (a_a1 <*> a))).run(c)
+        }
+    }
+
 
 //MARK: Result
+
     func testResultFunctorIdentity() {
         property("Result - Functor Laws - Identity") <- forAll { (x: String) in
             let a = Result<String,String>.success(x)
@@ -231,8 +276,19 @@ class LawsTests: XCTestCase {
         }
     }
 
+    func testResultApplicativeComposition() {
+        property("Result - Applicative Laws - Interchange") <- forAll { (af: ArrowOf<String,String>, ag: ArrowOf<String,String>, x: String) in
+            let a = Result<String,String>.pure(x)
+            let a_a1 = Result<String,Endo<String>>.pure(af.getArrow)
+            let a_a2 = Result<String,Endo<String>>.pure(ag.getArrow)
+            let a_a_a = Result<String,(@escaping Endo<String>) -> (@escaping Endo<String>) -> Endo<String>>.pure(fcurry(fcompose))
+            return ((a_a_a <*> a_a1 <*> a_a2 <*> a) == (a_a2 <*> (a_a1 <*> a)))
+        }
+    }
+
 
 //MARK: State
+
     func testStateFunctorIdentity() {
         property("State - Functor Laws - Identity") <- forAll { (x: String, c: String) in
             let a = State<String,String>.unfold { s in (s,x) }
@@ -275,8 +331,19 @@ class LawsTests: XCTestCase {
         }
     }
 
+    func testStateApplicativeComposition() {
+        property("State - Applicative Laws - Interchange") <- forAll { (af: ArrowOf<String,String>, ag: ArrowOf<String,String>, x: String, c: String) in
+            let a = State<String,String>.pure(x)
+            let a_a1 = State<String,Endo<String>>.pure(af.getArrow)
+            let a_a2 = State<String,Endo<String>>.pure(ag.getArrow)
+            let a_a_a = State<String,(@escaping Endo<String>) -> (@escaping Endo<String>) -> Endo<String>>.pure(fcurry(fcompose))
+            return ((a_a_a <*> a_a1 <*> a_a2 <*> a) == (a_a2 <*> (a_a1 <*> a))).run(c)
+        }
+    }
+
 
 //MARK: Writer
+
     func testWriterFunctorIdentity() {
         property("Writer - Functor Laws - Identity") <- forAll { (x: String) in
             let a = Writer<String,String>.init(log: .empty, value: x)
@@ -316,6 +383,16 @@ class LawsTests: XCTestCase {
             let a = Writer<String,String>.pure(x)
             let a_a_a = Writer<String,(@escaping Endo<String>) -> String>.pure { $0(x) }
             return ((a_a <*> a) == (a_a_a <*> a_a))
+        }
+    }
+
+    func testWriterApplicativeComposition() {
+        property("Writer - Applicative Laws - Interchange") <- forAll { (af: ArrowOf<String,String>, ag: ArrowOf<String,String>, x: String) in
+            let a = Writer<String,String>.pure(x)
+            let a_a1 = Writer<String,Endo<String>>.pure(af.getArrow)
+            let a_a2 = Writer<String,Endo<String>>.pure(ag.getArrow)
+            let a_a_a = Writer<String,(@escaping Endo<String>) -> (@escaping Endo<String>) -> Endo<String>>.pure(fcurry(fcompose))
+            return ((a_a_a <*> a_a1 <*> a_a2 <*> a) == (a_a2 <*> (a_a1 <*> a)))
         }
     }
 
