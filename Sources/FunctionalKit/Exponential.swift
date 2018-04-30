1ce61768c96c7ad1e0166fe6205fbcbc164d7b9a
Elviro Rocca <elviro.rocca@facile.it>
2018-04-30 17:35:29 +0200

    Add PredicateSet utilities to ExponentialType

--- Sources/FunctionalKit/Exponential.swift
+++ Sources/FunctionalKit/Exponential.swift
@@ -12,12 +12,14 @@ public protocol ExponentialType {
 
 extension Function: ExponentialType {}
 
+// MARK: - Functor
+
 extension ExponentialType {
 	public func dimap<A,B>(_ source: @escaping (A) -> SourceType, _ target: @escaping (TargetType) -> B) -> Function<A,B> {
 		return Function<A,B>.init { value in target(self.call(source(value))) }
 	}
 
-	public func toExponential() -> Function<SourceType,TargetType> {
+	public func toFunction() -> Function<SourceType,TargetType> {
 		return dimap(f.identity, f.identity)
 	}
 
@@ -30,11 +32,7 @@ extension ExponentialType {
 	}
 }
 
-extension ExponentialType where SourceType == TargetType {
-	public static func identity() -> Function<SourceType,TargetType> {
-		return Function.init { $0 }
-	}
-}
+//MARK: - Equatable
 
 extension ExponentialType where TargetType: Equatable {
     public static func == (lhs: Self, rhs: Self) -> Reader<SourceType,Bool> {
@@ -42,4 +40,58 @@ extension ExponentialType where TargetType: Equatable {
             lhs.call(source) == rhs.call(source)
         }
     }
+}
+
+//MARK: - PredicateSet
+
+public extension ExponentialType where TargetType == Bool {
+	static var universe: Function<SourceType,Bool> {
+		return Function<SourceType,Bool> { _ in true }
+	}
+
+	static var empty: Function<SourceType,Bool> {
+		return Function<SourceType,Bool> { _ in false }
+	}
+
+	func contains(_ value: SourceType) -> Bool {
+		return call(value)
+	}
+
+	func inverted() -> Function<SourceType,Bool> {
+		return Function<SourceType,Bool> {
+			self.contains($0).not
+		}
+	}
+
+	func union (_ other: Function<SourceType,Bool>) -> Function<SourceType,Bool> {
+		return Function<SourceType,Bool> {
+			self.contains($0) || other.contains($0)
+		}
+	}
+
+	func intersection (_ other: Function<SourceType,Bool>) -> Function<SourceType,Bool> {
+		return Function<SourceType,Bool> {
+			self.contains($0) && other.contains($0)
+		}
+	}
+
+	func minus (_ other: Function<SourceType,Bool>) -> Function<SourceType,Bool> {
+		return Function<SourceType,Bool> {
+			self.contains($0) && other.contains($0).not
+		}
+	}
+
+	func exclusiveDisjunction (_ other: Function<SourceType,Bool>) -> Function<SourceType,Bool> {
+		let unionSet = self.union(other)
+		let intersectionSet = self.intersection(other)
+		return unionSet.minus(intersectionSet)
+	}
+}
+
+//MARK: - Utility
+
+extension ExponentialType where SourceType == TargetType {
+	public static func identity() -> Function<SourceType,TargetType> {
+		return Function.init { $0 }
+	}
 }
