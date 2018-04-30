d7056e5cb5f37f80efceb528f7bb1ca6fb5b357a
Elviro Rocca <elviro.rocca@facile.it>
2018-04-30 15:47:22 +0200

    Fix tests

--- Tests/FunctionalKitTests/AffineTests.swift
+++ Tests/FunctionalKitTests/AffineTests.swift
@@ -16,20 +16,20 @@ class AffineTests: XCTestCase {
 	]
 
 	func testArrayAffineWellBehaved() {
-		property("TrySetTryGet") <- forAll { (aa: ArrayOf<Int>, v: Int, i: UInt) in
-			let array = aa.getArray
+		property("TrySetTryGet") <- forAll { (aa: Array<Int>, v: Int, i: UInt) in
+			let array = aa
 			let affine = type(of: array).affine(at: Int(i))
 			return AffineLaw.trySetTryGet(affine: affine, whole: array, part: v)
 		}
 
-		property("TryGetTrySet") <- forAll { (aa: ArrayOf<Int>, i: UInt) in
-			let array = aa.getArray
+		property("TryGetTrySet") <- forAll { (aa: Array<Int>, i: UInt) in
+			let array = aa
 			let affine = type(of: array).affine(at: Int(i))
 			return AffineLaw.tryGetTrySet(affine: affine, whole: array)
 		}
 
-		property("TrySetTrySet") <- forAll { (aa: ArrayOf<Int>, v: Int, i: UInt) in
-			let array = aa.getArray
+		property("TrySetTrySet") <- forAll { (aa: Array<Int>, v: Int, i: UInt) in
+			let array = aa
 			let affine = type(of: array).affine(at: Int(i))
 			return AffineLaw.trySetTrySet(affine: affine, whole: array, part: v)
 		}
