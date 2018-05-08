328bbfa18f75568e663714cce1ea6559b550d791
Elviro Rocca <elviro.rocca@facile.it>
2018-05-08 09:01:21 +0200

    Add "retrieve", "update" and "modify" effects to Reference

--- Sources/FunctionalKit/Reference.swift
+++ Sources/FunctionalKit/Reference.swift
@@ -1,3 +1,5 @@
+import Abstract
+
 public final class Ref<T> {
 	public var value: T {
 		didSet {
@@ -26,4 +28,26 @@ public final class Ref<T> {
 	}
 
 	private var listeners: [AnyHashable : (T) -> ()] = [:]
+}
+
+public extension Ref {
+	var retrieve: Effect<T> {
+		return Effect<T>.unfold { [unowned self] in
+			self.value
+		}
+	}
+
+	var update: Coeffect<T> {
+		return Coeffect<T>.unfold { [unowned self] newValue in
+			self.value = newValue
+		}
+	}
+}
+
+public extension Ref where T: Semigroup {
+	var modify: Coeffect<T> {
+		return Coeffect<T>.unfold { [unowned self] newValue in
+			self.value = self.value <> newValue
+		}
+	}
 }
