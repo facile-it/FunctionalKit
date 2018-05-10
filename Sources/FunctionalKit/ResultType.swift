3fafe176f2b4bc90d7e3fabfa4912a62c978098b
Elviro Rocca <elviro.rocca@facile.it>
2018-05-10 10:14:47 +0200

    Result

--- Sources/FunctionalKit/ResultType.swift
+++ Sources/FunctionalKit/ResultType.swift
@@ -1,7 +1,226 @@
-//#if SWIFT_PACKAGE
-//	import Operadics
-//#endif
-//import Abstract
+#if SWIFT_PACKAGE
+    import Operadics
+#endif
+import Abstract
+
+public enum Result<Failure,Parameter> where Failure: Error {
+    case success(Parameter)
+    case failure(Failure)
+    
+    public func run() throws -> Parameter {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+    public func fold <A> (onSuccess: (Parameter) -> A, onFailure: (Failure) -> A) -> A {
+        switch self {
+        case .success(let value):
+            return onSuccess(value)
+        case .failure(let error):
+            return onFailure(error)
+        }
+    }
+}
+
+extension Result: TypeConstructor2 {
+    public typealias ParameterType = Parameter
+    public typealias SecondaryType = Failure
+}
+
+extension Result: PureConstructible {
+    public static func pure(_ value: ParameterType) -> Result {
+        return Result.success(value)
+    }
+}
+
+public extension Result {
+    typealias Generic<F,A> = Result<F,A> where F: Error
+}
+
+public extension Result {
+    func map <A> (_ transform: (ParameterType) -> A) -> Result<Failure,A> {
+        switch self {
+        case let .success(value):
+            return .success(transform(value))
+        case let .failure(error):
+            return .failure(error)
+        }
+    }
+    
+    func mapError <A> (_ transform: (Failure) -> A) -> Result<A,ParameterType> {
+        switch self {
+        case let .success(value):
+            return .success(value)
+        case let .failure(error):
+            return .failure(transform(error))
+        }
+
+    }
+    
+    static func lift <A> (_ function: @escaping (ParameterType) -> A) -> (Result) -> Result<Failure,A> {
+        return { $0.map(function) }
+    }
+    
+    static func zip <F1,A,F2,B> (_ first: Result<F1,A>, _ second: Result<F2,B>) -> Result<Inclusive<F1,F2>,(A,B)> where Failure == Inclusive<F1,F2>, ParameterType == (A,B) {
+        switch (first, second) {
+        case let (.success(leftValue), .success(rightValue)):
+            return .success((leftValue,rightValue))
+        case let (.failure(leftError), .failure(rightError)):
+            return .failure(.center(leftError,rightError))
+        case let (.failure(error), _):
+            return .failure(.left(error))
+        case let (_, .failure(error)):
+            return .failure(.right(error))
+        }
+    }
+
+    static func zipMerged <A,B> (_ first: Result<Failure,A>, _ second: Result<Failure,B>) -> Result<Failure,(A,B)> where Failure: Semigroup, ParameterType == (A,B) {
+        return Generic.zip(first, second).mapError { $0.merged() }
+    }
+
+	func apply <A> (_ transform: Result<Failure,(ParameterType) -> A>) -> Result<Failure,A> {
+		return Generic.zip(self, transform)
+			.map { value, function in function(value) }
+			.mapError { $0.left }
+	}
+
+	func applyMerged <A> (_ transform: Result<Failure,(ParameterType) -> A>) -> Result<Failure,A> where Failure: Semigroup {
+		return Generic.zipMerged(self, transform)
+			.map { value, function in function(value) }
+	}
+
+	static func <*> <A> (lhs: Result<Failure,(ParameterType) -> A>, rhs: Result) -> Result<Failure,A> {
+		return rhs.apply(lhs)
+	}
+
+	static func <*> <A> (lhs: Result<Failure,(ParameterType) -> A>, rhs: Result) -> Result<Failure,A> where Failure: Semigroup {
+		return rhs.applyMerged(lhs)
+	}
+
+	static func lift <A,B> (_ function: @escaping (A, B) -> ParameterType) -> (Result<Failure,A>, Result<Failure,B>) -> Result {
+		return { ap1, ap2 in
+			Generic.pure(f.curry(function)) <*> ap1 <*> ap2
+		}
+	}
+
+	static func lift <A,B,C> (_ function: @escaping (A, B, C) -> ParameterType) -> (Result<Failure,A>, Result<Failure,B>, Result<Failure,C>) -> Result {
+		return { ap1, ap2, ap3 in
+			Generic.pure(f.curry(function)) <*> ap1 <*> ap2 <*> ap3
+		}
+	}
+
+	func joined<A>() -> Result<Failure,A> where ParameterType == Result<Failure,A> {
+		switch self {
+		case let .success(value):
+			return value
+		case let .failure(error):
+			return .failure(error)
+		}
+	}
+
+	func flatMap <A> (_ transform: (ParameterType) -> Result<Failure,A>) -> Result<Failure,A> {
+		return map(transform).joined()
+	}
+
+	func traverse <A> (_ transform: (ParameterType) -> Array<A>) -> Array<Result<Failure,A>> {
+		switch self {
+		case let .success(value):
+			return Array.pure(Generic.success) <*> transform(value)
+		case let .failure(error):
+			return Array.pure(Generic.failure(error))
+		}
+	}
+
+	func traverse <A> (_ transform: (ParameterType) -> Effect<A>) -> Effect<Result<Failure,A>> {
+		switch self {
+		case let .success(value):
+			return Effect.pure(Generic.success) <*> transform(value)
+		case let .failure(error):
+			return Effect.pure(Generic.failure(error))
+		}
+	}
+
+	func traverse <A> (_ transform: (ParameterType) -> Future<A>) -> Future<Result<Failure,A>> {
+		switch self {
+		case let .success(value):
+			return Future.pure(Generic.success) <*> transform(value)
+		case let .failure(error):
+			return Future.pure(Generic.failure(error))
+		}
+	}
+
+	func traverse <A> (_ transform: (ParameterType) -> Optional<A>) -> Optional<Result<Failure,A>> {
+		switch self {
+		case let .success(value):
+			return Optional.pure(Generic.success) <*> transform(value)
+		case let .failure(error):
+			return Optional.pure(Generic.failure(error))
+		}
+	}
+
+	func traverse <A,E> (_ transform: (ParameterType) -> Reader<E,A>) -> Reader<E,Result<Failure,A>> {
+		switch self {
+		case let .success(value):
+			return Reader.pure(Generic.success) <*> transform(value)
+		case let .failure(error):
+			return Reader.pure(Generic.failure(error))
+		}
+	}
+
+	func traverse <A,F> (_ transform: (ParameterType) -> Result<F,A>) -> Result<F,Result<Failure,A>> {
+		switch self {
+		case let .success(value):
+			return Generic.pure(Generic.success) <*> transform(value)
+		case let .failure(error):
+			return Generic.pure(Generic.failure(error))
+		}
+	}
+
+	func toOptionalError() -> Failure? {
+		switch self {
+		case let .failure(error):
+			return .some(error)
+		case .success:
+			return .none
+		}
+	}
+
+	func toOptionalValue() -> ParameterType? {
+		switch self {
+		case let .success(value):
+			return .some(value)
+		case .failure:
+			return .none
+		}
+	}
+
+	func fallback(to defaultValue: @autoclosure () -> ParameterType) -> Result {
+		switch self {
+		case let .success(value):
+			return .success(value)
+		case .failure:
+			return .success(defaultValue())
+		}
+	}
+
+	static func getFromThrowing(getError: @escaping (Error) -> Failure) -> (() throws -> ParameterType) -> Result {
+		return { throwing in
+			do {
+				return try .success(throwing())
+			}
+			catch let error {
+				return .failure(getError(error))
+			}
+		}
+	}
+}
+
+
+
 //
 //// MARK: - Definiton
 //
@@ -33,33 +252,33 @@
 //// sourcery: testConstruct = "success(x)"
 //// sourcery: testSecondaryParameter
 //public enum Result<E,T>: ResultType where E: Error {
-//	public typealias ErrorType = E
-//	public typealias ParameterType = T
+//    public typealias ErrorType = E
+//    public typealias ParameterType = T
 //
-//	case success(T)
-//	case failure(E)
+//    case success(T)
+//    case failure(E)
 //
-//	public static func from(concrete: Result<E, T>) -> Result<E, T> {
-//		return concrete
-//	}
-//	
-//	public func run() throws -> T {
-//		switch self {
-//		case .success(let value):
-//			return value
-//		case .failure(let error):
-//			throw error
-//		}
-//	}
+//    public static func from(concrete: Result<E, T>) -> Result<E, T> {
+//        return concrete
+//    }
+//    
+//    public func run() throws -> T {
+//        switch self {
+//        case .success(let value):
+//            return value
+//        case .failure(let error):
+//            throw error
+//        }
+//    }
 //
-//	public func fold<A>(onSuccess: (T) -> A, onFailure: (E) -> A) -> A {
-//		switch self {
-//		case .success(let value):
-//			return onSuccess(value)
-//		case .failure(let error):
-//			return onFailure(error)
-//		}
-//	}
+//    public func fold<A>(onSuccess: (T) -> A, onFailure: (E) -> A) -> A {
+//        switch self {
+//        case .success(let value):
+//            return onSuccess(value)
+//        case .failure(let error):
+//            return onFailure(error)
+//        }
+//    }
 //}
 //
 //// MARK: - Concrete
@@ -75,22 +294,22 @@
 //// MARK: - Functor
 //
 //public extension ResultType {
-//	func map <A> (_ transform: (ParameterType) -> A) -> Result<ErrorType,A> {
-//		return withoutActuallyEscaping(transform) { transform in
-//			fold(
-//				onSuccess: transform >>> Result.success,
-//				onFailure: Result.failure)
-//		}
-//	}
+//    func map <A> (_ transform: (ParameterType) -> A) -> Result<ErrorType,A> {
+//        return withoutActuallyEscaping(transform) { transform in
+//            fold(
+//                onSuccess: transform >>> Result.success,
+//                onFailure: Result.failure)
+//        }
+//    }
 //
-//	func mapError <A> (_ transform: (ErrorType) -> A) -> Result<A,ParameterType> {
-//		return withoutActuallyEscaping(transform) { transform in
-//			fold(
-//				onSuccess: Result.success,
-//				onFailure: transform >>> Result.failure)
-//		}
-//	}
-//    
+//    func mapError <A> (_ transform: (ErrorType) -> A) -> Result<A,ParameterType> {
+//        return withoutActuallyEscaping(transform) { transform in
+//            fold(
+//                onSuccess: Result.success,
+//                onFailure: transform >>> Result.failure)
+//        }
+//    }
+//
 //    static func lift<A>(_ function: @escaping (ParameterType) -> A) -> (Self) -> Result<ErrorType,A> {
 //        return { $0.map(function) }
 //    }
@@ -100,7 +319,7 @@
 //            Concrete.pure(f.curry(function)) <*> ap1 <*> ap2
 //        }
 //    }
-//    
+//
 //    static func lift<A,Applicative2,Applicative3>(_ function: @escaping (ParameterType, Applicative2.ParameterType, Applicative3.ParameterType) -> A) -> (Self, Applicative2, Applicative3) -> Result<ErrorType, A> where Applicative2: ResultType, Applicative3: ResultType, Applicative2.ErrorType == ErrorType, Applicative3.ErrorType == ErrorType {
 //        return { ap1, ap2, ap3 in
 //            Concrete.pure(f.curry(function)) <*> ap1 <*> ap2 <*> ap3
@@ -113,27 +332,27 @@
 //public extension ResultType {
 //	typealias Zipped<R1,R2> = Result<Inclusive<R1.ErrorType,R2.ErrorType>,(R1.ParameterType,R2.ParameterType)> where R1: ResultType, R2: ResultType
 //
-//	static func zip<R1,R2>(_ first: R1, _ second: R2) -> Zipped<R1,R2> where R1: ResultType, R2: ResultType, ParameterType == (R1.ParameterType,R2.ParameterType), ErrorType == Inclusive<R1.ErrorType,R2.ErrorType> {
-//		return first.fold(
-//			onSuccess: { firstValue in
-//				second.fold(
-//					onSuccess: { secondValue in
-//						Zipped<R1,R2>.success((firstValue,secondValue))
-//				},
-//					onFailure: { secondError in
-//						Zipped<R1,R2>.failure(Inclusive.right(secondError))
-//				})
-//		},
-//			onFailure: { firstError in
-//				second.fold(
-//					onSuccess: { _ in
-//						Zipped<R1,R2>.failure(Inclusive.left(firstError))
-//				},
-//					onFailure: { secondError in
-//						Zipped<R1,R2>.failure(Inclusive.center(firstError, secondError))
-//				})
-//		})
-//	}
+//    static func zip<R1,R2>(_ first: R1, _ second: R2) -> Zipped<R1,R2> where R1: ResultType, R2: ResultType, ParameterType == (R1.ParameterType,R2.ParameterType), ErrorType == Inclusive<R1.ErrorType,R2.ErrorType> {
+//        return first.fold(
+//            onSuccess: { firstValue in
+//                second.fold(
+//                    onSuccess: { secondValue in
+//                        Zipped<R1,R2>.success((firstValue,secondValue))
+//                },
+//                    onFailure: { secondError in
+//                        Zipped<R1,R2>.failure(Inclusive.right(secondError))
+//                })
+//        },
+//            onFailure: { firstError in
+//                second.fold(
+//                    onSuccess: { _ in
+//                        Zipped<R1,R2>.failure(Inclusive.left(firstError))
+//                },
+//                    onFailure: { secondError in
+//                        Zipped<R1,R2>.failure(Inclusive.center(firstError, secondError))
+//                })
+//        })
+//    }
 //}
 //
 //// MARK: - Applicative
@@ -154,7 +373,7 @@
 //			.map { function, value in function(value) }
 //			.mapError { $0.left }
 //	}
-//    
+//
 //}
 //
 //public extension ResultType where ErrorType: Semigroup {
