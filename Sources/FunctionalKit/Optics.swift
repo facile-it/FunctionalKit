99cbb0b6702e7f4dacf9b15448af320eb5db25b5
Elviro Rocca <elviro.rocca@facile.it>
2018-05-15 17:50:47 +0200

    Remove OpticsType protocol

--- Sources/FunctionalKit/Optics.swift
+++
@@ -1,8 +0,0 @@
-/// A "OpticsType" is a type with references to a "Whole" and a "Part"
-
-public protocol OpticsType {
-    associatedtype SType
-    associatedtype TType
-    associatedtype AType
-    associatedtype BType
-}
