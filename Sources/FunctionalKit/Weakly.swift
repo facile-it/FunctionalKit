9e1b4719c611e49cea05ea336e443fed80adadd5
Elviro Rocca <elviro.rocca@facile.it>
2018-08-20 10:00:35 +0200

    Better naming for f.weakly

--- Sources/FunctionalKit/Weakly.swift
+++ Sources/FunctionalKit/Weakly.swift
@@ -5,35 +5,42 @@ import Abstract
 
 public extension f {
 
-	static func weakly<T,I,O>(_ reference: T?, onNil onNilValue: @autoclosure @escaping () -> O, closure: @escaping (T,I) -> O) -> (I) -> O where T: AnyObject {
-		return { [weak reference] input in
-			guard let some = reference else { return onNilValue() }
-			return closure(some,input)
-		}
-	}
-
-	static func weakly<T,I1,I2,O>(_ reference: T?, onNil onNilValue: @autoclosure @escaping () -> O, closure: @escaping (T,I1,I2) -> O) -> (I1,I2) -> O where T: AnyObject {
-		return { [weak reference] (input1, input2) in
-			guard let some = reference else { return onNilValue() }
-			return closure(some,input1,input2)
-		}
-	}
-
-	static func weakly<T,I1,I2,I3,O>(_ reference: T?, onNil onNilValue: @autoclosure @escaping () -> O, closure: @escaping (T,I1,I2,I3) -> O) -> (I1,I2,I3) -> O where T: AnyObject {
-		return { [weak reference] (input1, input2, input3) in
-			guard let some = reference else { return onNilValue() }
-			return closure(some,input1,input2,input3)
-		}
-	}
-
-	static func weakly<T,O>(_ reference: T?, onNil onNilValue: @autoclosure @escaping () -> O, closure: @escaping (T) -> O) -> () -> O where T: AnyObject {
+	static func weaklyOr<T,O>(_ reference: T?, onNil onNilValue: @autoclosure @escaping () -> O, closure: @escaping (T) -> O) -> () -> O where T: AnyObject {
 		return { [weak reference] in
 			guard let some = reference else { return onNilValue() }
 			return closure(some)
 		}
 	}
 
-	static func weakly<T,I>(_ reference: T?, closure: @escaping (T,I) -> ()) -> (I) -> () where T: AnyObject {
+	static func weaklyOr<T,I1,O>(_ reference: T?, onNil onNilValue: @autoclosure @escaping () -> O, closure: @escaping (T,I1) -> O) -> (I1) -> O where T: AnyObject {
+		return { [weak reference] input in
+			guard let some = reference else { return onNilValue() }
+			return closure(some,input)
+		}
+	}
+
+	static func weaklyOr<T,I1,I2,O>(_ reference: T?, onNil onNilValue: @autoclosure @escaping () -> O, closure: @escaping (T,I1,I2) -> O) -> (I1,I2) -> O where T: AnyObject {
+		return { [weak reference] (input1, input2) in
+			guard let some = reference else { return onNilValue() }
+			return closure(some,input1,input2)
+		}
+	}
+
+	static func weaklyOr<T,I1,I2,I3,O>(_ reference: T?, onNil onNilValue: @autoclosure @escaping () -> O, closure: @escaping (T,I1,I2,I3) -> O) -> (I1,I2,I3) -> O where T: AnyObject {
+		return { [weak reference] (input1, input2, input3) in
+			guard let some = reference else { return onNilValue() }
+			return closure(some,input1,input2,input3)
+		}
+	}
+
+	static func weakly<T>(_ reference: T?, closure: @escaping (T) -> ()) -> () -> () where T: AnyObject {
+		return { [weak reference] in
+			guard let some = reference else { return }
+			closure(some)
+		}
+	}
+
+	static func weakly<T,I1>(_ reference: T?, closure: @escaping (T,I1) -> ()) -> (I1) -> () where T: AnyObject {
 		return { [weak reference] input in
 			guard let some = reference else { return }
 			closure(some,input)
@@ -51,13 +58,6 @@ public extension f {
 		return { [weak reference] (input1, input2, input3) in
 			guard let some = reference else { return }
 			closure(some,input1,input2,input3)
-		}
-	}
-
-	static func weakly<T>(_ reference: T?, closure: @escaping (T) -> ()) -> () -> () where T: AnyObject {
-		return { [weak reference] in
-			guard let some = reference else { return }
-			closure(some)
 		}
 	}
 }
