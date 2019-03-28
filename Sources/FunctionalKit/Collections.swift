e88e6c3e329cd8af8a0c46dab54a8d1c8bed1612
Christian Miranti <christian.miranti@gmail.com>
2019-03-28 12:02:50 +0100

    Safely uncommented part of Collections

--- Sources/FunctionalKit/Collections.swift
+++ Sources/FunctionalKit/Collections.swift
@@ -1,9 +1,9 @@
-//import Foundation
-//#if SWIFT_PACKAGE
-//    import Operadics
-//#endif
-//import Abstract
-//
+import Foundation
+#if SWIFT_PACKAGE
+    import Operadics
+#endif
+import Abstract
+
 //// MARK: - Decomposition
 //
 //public extension Sequence where SubSequence: Sequence, SubSequence.Iterator.Element == Iterator.Element {
@@ -25,31 +25,31 @@
 //        return try nonOptTail.reduce(nonOptHead, combine)
 //    }
 //}
-//
-//public extension RandomAccessCollection {
-//    func destructured2(_ dropIfNeeded: Bool = false) -> (Element, Element)? {
-//        let checkCount = dropIfNeeded ? { $0 >= 2 } : { $0 == 2 }
-//
-//        guard checkCount(count) else { return nil }
-//
-//        return (
-//            self[startIndex],
-//            self[index(after: startIndex)]
-//        )
-//    }
-//
-//    func destructured3(_ dropIfNeeded: Bool = false) -> (Element, Element, Element)? {
-//        let checkCount = dropIfNeeded ? { $0 >= 3 } : { $0 == 3 }
-//
-//        guard checkCount(count) else { return nil }
-//
-//        return (
-//            self[startIndex],
-//            self[index(after: startIndex)],
-//            self[index(after: index(after: startIndex))]
-//        )
-//    }
-//}
+
+public extension RandomAccessCollection {
+    func destructured2(_ dropIfNeeded: Bool = false) -> (Element, Element)? {
+        let checkCount = dropIfNeeded ? { $0 >= 2 } : { $0 == 2 }
+
+        guard checkCount(count) else { return nil }
+
+        return (
+            self[startIndex],
+            self[index(after: startIndex)]
+        )
+    }
+
+    func destructured3(_ dropIfNeeded: Bool = false) -> (Element, Element, Element)? {
+        let checkCount = dropIfNeeded ? { $0 >= 3 } : { $0 == 3 }
+
+        guard checkCount(count) else { return nil }
+
+        return (
+            self[startIndex],
+            self[index(after: startIndex)],
+            self[index(after: index(after: startIndex))]
+        )
+    }
+}
 //
 //public extension RandomAccessCollection where Element: Equatable {
 //    func divideIn2(atIndex index: Index) -> ([Element],[Element]) {
@@ -67,49 +67,49 @@
 //            .get(or: (Array(self),[]))
 //    }
 //}
-//
-//// MARK: - Query
-//
-//public extension Sequence {
-//    func all(conformTo predicate: @escaping (Element) -> Bool) -> Bool {
-//        return map(predicate >>> And.init(_:)).concatenated().unwrap
-//    }
-//
-//    func any(conformsTo predicate: @escaping (Element) -> Bool) -> Bool {
-//        return map(predicate >>> Or.init(_:)).concatenated().unwrap
-//    }
-//}
-//
-//public extension Sequence where Iterator.Element == Bool {
-//    var isAllTrue: Bool {
-//        return all(conformTo: f.identity)
-//    }
-//
-//    var isAnyTrue: Bool {
-//        return any(conformsTo: f.identity)
-//    }
-//}
-//
-//public extension Sequence where Iterator.Element: CoproductType {
-//    func partition() -> Product<[Iterator.Element.LeftType], [Iterator.Element.RightType]> {
-//
-//        let initial = Product<[Iterator.Element.LeftType], [Iterator.Element.RightType]>.init([], [])
-//
-//        return self.reduce(initial) { reducer, coproduct in
-//            return coproduct.fold(
-//                onLeft: { left in reducer.mapFirst { $0.appending(left) }},
-//                onRight: { right in reducer.mapSecond { $0.appending(right) }})
-//        }
-//    }
-//}
-//
-//public extension RandomAccessCollection {
-//    func getSafely(at index: Index) -> Iterator.Element? {
-//        guard indices.contains(index) else { return nil }
-//        return self[index]
-//    }
-//}
-//
+
+// MARK: - Query
+
+public extension Sequence {
+    func all(conformTo predicate: @escaping (Element) -> Bool) -> Bool {
+        return map(predicate >>> And.init(_:)).concatenated().unwrap
+    }
+
+    func any(conformsTo predicate: @escaping (Element) -> Bool) -> Bool {
+        return map(predicate >>> Or.init(_:)).concatenated().unwrap
+    }
+}
+
+public extension Sequence where Iterator.Element == Bool {
+    var isAllTrue: Bool {
+        return all(conformTo: f.identity)
+    }
+
+    var isAnyTrue: Bool {
+        return any(conformsTo: f.identity)
+    }
+}
+
+public extension Sequence where Iterator.Element: CoproductType {
+    func partition() -> Product<[Iterator.Element.LeftType], [Iterator.Element.RightType]> {
+
+        let initial = Product<[Iterator.Element.LeftType], [Iterator.Element.RightType]>.init([], [])
+
+        return self.reduce(initial) { reducer, coproduct in
+            return coproduct.fold(
+                onLeft: { left in reducer.mapFirst { $0.appending(left) }},
+                onRight: { right in reducer.mapSecond { $0.appending(right) }})
+        }
+    }
+}
+
+public extension RandomAccessCollection {
+    func getSafely(at index: Index) -> Iterator.Element? {
+        guard indices.contains(index) else { return nil }
+        return self[index]
+    }
+}
+
 // MARK: - Construction
 
 public extension RangeReplaceableCollection {
