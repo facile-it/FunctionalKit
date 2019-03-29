0f97ba5a88e077a51a29858f6996849db2560a31
Christian Miranti <christian.miranti@gmail.com>
2019-03-29 15:06:18 +0100

    Isolate Segmentation faults on Tests

--- Tests/FunctionalKitTests/WeaklyTests.swift
+++ Tests/FunctionalKitTests/WeaklyTests.swift
@@ -1,20 +1,20 @@
-//import XCTest
-//@testable import FunctionalKit
-//#if SWIFT_PACKAGE
-//    import Operadics
-//#endif
-//import Abstract
-//
-//class WeaklyTests: XCTestCase {
-//    func testWeaklyNone() {
-//        var ref = Optional.some(Ref<Int>.init(42))
-//        ref = nil
-//
-//        f.weakly(ref) { _ in
-//            notExpected()
-//        }()
-//    }
-//
+import XCTest
+@testable import FunctionalKit
+#if SWIFT_PACKAGE
+    import Operadics
+#endif
+import Abstract
+
+class WeaklyTests: XCTestCase {
+    func testWeaklyNone() {
+        var ref = Optional.some(Ref<Int>.init(42))
+        ref = nil
+
+        f.weakly(ref) { _ in
+            notExpected()
+        }()
+    }
+
 //    func testWeaklyNoneIO() {
 //        var ref = Optional.some(Ref<Int>.init(42))
 //        ref = nil
@@ -23,19 +23,19 @@
 //            notExpected()
 //        }
 //    }
-//
-//
-//    func testWeaklySomeIO() {
-//        let ref = Optional.some(Ref<Int>.init(42))
-//
-//        42 |> f.weakly(ref) { this, value in
-//            value ==! this.value
-//        }
-//    }
-//
-//    static var allTests = [
-//        ("testWeaklyNone", testWeaklyNone),
+
+
+    func testWeaklySomeIO() {
+        let ref = Optional.some(Ref<Int>.init(42))
+
+        42 |> f.weakly(ref) { this, value in
+            value ==! this.value
+        }
+    }
+
+    static var allTests = [
+        ("testWeaklyNone", testWeaklyNone),
 //        ("testWeaklyNoneIO", testWeaklyNoneIO),
-//        ("testWeaklySomeIO", testWeaklySomeIO),
-//        ]
-//}
+        ("testWeaklySomeIO", testWeaklySomeIO),
+        ]
+}
