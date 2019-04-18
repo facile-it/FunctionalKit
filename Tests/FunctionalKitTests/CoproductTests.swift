481bebebd893192209fcea66c7e2bd44e351202c
Andrea Rinaldi <andrea.rinaldi91@gmail.com>
2019-04-18 17:15:16 +0200

    Add toBool to extension of CoproductType.

--- Tests/FunctionalKitTests/CoproductTests.swift
+++ Tests/FunctionalKitTests/CoproductTests.swift
@@ -6,39 +6,52 @@ import SwiftCheck
 	import Operadics
 #endif
 
-typealias TestType = Coproduct<Int,String>
+typealias TestTypeIntString = Coproduct<Int,String>
+
+typealias False = Bool
+typealias True = Bool
+typealias TestTypeBool = Coproduct<False,True>
 
 class CoproductTests: XCTestCase {
 	func testTry() {
-		TestType.left(42).tryLeft()==!
-		TestType.left(42).tryRight()==?
+		TestTypeIntString.left(42).tryLeft()==!
+		TestTypeIntString.left(42).tryRight()==?
 
-		TestType.right("42").tryLeft()==?
-		TestType.right("42").tryRight()==!
+		TestTypeIntString.right("42").tryLeft()==?
+		TestTypeIntString.right("42").tryRight()==!
 	}
 
 	func testFold() {
-		TestType.left(42).fold(onLeft: f.pure(true), onRight: f.pure(false)) ==! true
-		TestType.right("42").fold(onLeft: f.pure(false), onRight: f.pure(true)) ==! true
+		TestTypeIntString.left(42).fold(onLeft: f.pure(true), onRight: f.pure(false)) ==! true
+		TestTypeIntString.right("42").fold(onLeft: f.pure(false), onRight: f.pure(true)) ==! true
 
-		TestType.left(42).foldToLeft { Int($0)! } ==! 42
-		TestType.right("42").foldToRight { "\($0)" } ==! "42"
+		TestTypeIntString.left(42).foldToLeft { Int($0)! } ==! 42
+		TestTypeIntString.right("42").foldToRight { "\($0)" } ==! "42"
 	}
 
 	func testMap() {
-		TestType.left(42).bimap(f.identity, { Int($0)! }).tryLeft()! ==! 42
-		TestType.right("42").bimap({ "\($0)" }, f.identity).tryRight()! ==! "42"
+		TestTypeIntString.left(42).bimap(f.identity, { Int($0)! }).tryLeft()! ==! 42
+		TestTypeIntString.right("42").bimap({ "\($0)" }, f.identity).tryRight()! ==! "42"
 
-		TestType.left(42).mapLeft { $0*2 }.tryLeft()! ==! 84
-		TestType.left(42).mapRight { $0 + "!" }.tryLeft()! ==! 42
+		TestTypeIntString.left(42).mapLeft { $0*2 }.tryLeft()! ==! 84
+		TestTypeIntString.left(42).mapRight { $0 + "!" }.tryLeft()! ==! 42
 
-		TestType.right("42").mapLeft { $0*2 }.tryRight()! ==! "42"
-		TestType.right("42").mapRight { $0 + "!" }.tryRight()! ==! "42!"
+		TestTypeIntString.right("42").mapLeft { $0*2 }.tryRight()! ==! "42"
+		TestTypeIntString.right("42").mapRight { $0 + "!" }.tryRight()! ==! "42!"
 	}
 
+    func testToBool() {
+        TestTypeBool.left(false).tryLeft()==!
+        TestTypeBool.left(false).tryRight()==?
+        
+        TestTypeBool.right(true).tryLeft()==?
+        TestTypeBool.right(true).tryRight()==!
+    }
+    
 	static var allTests = [
 		("testTry", testTry),
 		("testFold", testFold),
 		("testMap", testMap),
+        ("testToBool", testToBool)
 		]
 }
